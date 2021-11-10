3c4dd70acb76c39e148e9c5c03ba2eaf0bda6a4c
Luke Nguyen <luke.nguyen.goldenowl@gmail.com>
2021-11-10 16:43:49 +0700

    update stack_and_align.dart

--- lib/screens/stack_and_align.dart
+++ lib/screens/stack_and_align.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 
-class StackAndAlign extends StatelessWidget {
+class StackAndAlign extends StatefulWidget {
   static const String routeName = '/stack-&-align';
 
   const StackAndAlign({Key? key}) : super(key: key);
+
+  @override
+  _StackAndAlignState createState() => _StackAndAlignState();
+}
+
+class _StackAndAlignState extends State<StackAndAlign> {
+  final List<Alignment> _listAlign = [
+    Alignment.center,
+    Alignment.centerLeft,
+    Alignment.centerRight,
+    Alignment.topCenter,
+    Alignment.topLeft,
+    Alignment.topRight,
+    Alignment.bottomCenter,
+    Alignment.bottomLeft,
+    Alignment.bottomRight,
+  ];
+  Alignment _align = Alignment.center;
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +35,7 @@ class StackAndAlign extends StatelessWidget {
             child: Container(
               alignment: Alignment.center,
               child: Stack(
-                alignment: Alignment.center,
+                alignment: _align,
                 children: [
                   Container(
                     height: 300,
@@ -39,9 +57,48 @@ class StackAndAlign extends StatelessWidget {
             ),
           ),
           Container(
-            height: 100,
-            width: 100,
-            color: Colors.red,
+            decoration: BoxDecoration(
+              border: Border.all(width: 0.03),
+            ),
+            child: _myDrop<Alignment>(
+              _align,
+              _listAlign,
+              onChangeValue: (val) {
+                setState(() {
+                  _align = val;
+                });
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _myDrop<T>(T myValue, List<T> myList, {required onChangeValue}) {
+    return Padding(
+      padding: const EdgeInsets.all(17),
+      child: Row(
+        children: [
+          Expanded(
+            child: Text(
+              myValue.runtimeType.toString(),
+              style: const TextStyle(fontSize: 17),
+            ),
+          ),
+          DropdownButton<T>(
+            borderRadius: BorderRadius.circular(10),
+            value: myValue,
+            icon: const Icon(Icons.arrow_drop_down),
+            iconSize: 30,
+            elevation: 16,
+            onChanged: onChangeValue,
+            items: myList.map((T value) {
+              return DropdownMenuItem<T>(
+                value: value,
+                child: Text(value.toString().split('.').last),
+              );
+            }).toList(),
           ),
         ],
       ),
