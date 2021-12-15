4beb11fa9a97d6eb59a9b4551078e9a07d5ae9b8
Luke Nguyen <luke.nguyen.goldenowl@gmail.com>
2021-12-15 14:33:35 +0700

    Update Typography Screen and custom fonts

--- lib/screens/typography.dart
+++ lib/screens/typography.dart
@@ -10,341 +10,112 @@ class TypographyScreen extends StatefulWidget {
 }
 
 class _TypographyScreenState extends State<TypographyScreen> {
-  final double _lineSpace = 15;
+  final Map<String, String> _fontWeightType = {
+    'w100': 'Light',
+    'w300': 'Light',
+    'w400': 'Regular',
+    'w500': 'Medium',
+  };
 
   Widget _line() {
-    return Padding(
-      padding: EdgeInsets.only(top: _lineSpace, bottom: _lineSpace),
-      child: Container(
+    return const Padding(
+      padding: EdgeInsets.only(bottom: 15, top: 5),
+      child: Divider(
         height: 1,
-        color: Colors.grey,
       ),
     );
   }
 
+  Widget _buildText(ItemData? itemData) {
+    return Column(
+      children: [
+        ListTile(
+          title: Text(
+            itemData!.title,
+            style: itemData.style,
+          ),
+          subtitle: Text(itemData.description),
+          trailing: Text(
+            (_getFontWeightType(itemData.style) ?? '') +
+                ' ' +
+                itemData.style!.fontSize!.floor().toString(),
+          ),
+        ),
+        _line(),
+      ],
+    );
+  }
+
+  String? _getFontWeightType(TextStyle? style) {
+    String _fonWeight = style!.fontWeight.toString().split('.').last;
+    return _fontWeightType[_fonWeight];
+  }
+
   @override
   Widget build(BuildContext context) {
+    var _textTheme = Theme.of(context).textTheme;
+    List<ItemData> itemData = [
+      ItemData('H1', '', _textTheme.headline1),
+      ItemData(
+          'H2',
+          'Used for the date in the dialog shown by [showDatePicker].',
+          _textTheme.headline2),
+      ItemData('H3', '', _textTheme.headline3),
+      ItemData('H4', '', _textTheme.headline4),
+      ItemData(
+          'H5',
+          'Used for large text in dialogs (e.g., the month and year in the dialog shown by [showDatePicker]).',
+          _textTheme.headline5),
+      ItemData(
+          'H6',
+          'Used for the primary text in app bars and dialogs (e.g., [AppBar.title] and [AlertDialog.title]).',
+          _textTheme.headline6),
+      ItemData(
+          'Subtitle1',
+          'Used for the primary text in lists (e.g., [ListTile.title]).',
+          _textTheme.subtitle1),
+      ItemData(
+          'Subtitle2',
+          'For medium emphasis text thatư\'s a little smaller than [subtitle1].',
+          _textTheme.subtitle2),
+      ItemData(
+          'BodyText1',
+          'Used for emphasizing text that would otherwise be [bodyText2].',
+          _textTheme.bodyText1),
+      ItemData('BodyText2', 'The default text style for [Material].',
+          _textTheme.bodyText2),
+      ItemData('Caption', 'Used for auxiliary text associated with images',
+          _textTheme.caption),
+      ItemData(
+          'Button',
+          'Used for text on [ElevatedButton], [TextButton] and [OutlinedButton].',
+          _textTheme.button),
+      ItemData(
+          'Overline',
+          'The smallest style. Typically used for captions or to introduce a (larger) headline.',
+          _textTheme.overline),
+    ];
+    print(_textTheme.headline1!.fontWeight.toString());
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Typography'),
       ),
       body: ListView(
-        padding: const EdgeInsets.all(10),
+        //padding: const EdgeInsets.all(10),
         children: [
-          Row(
-            children: [
-              Expanded(
-                child: Text(
-                  'H1',
-                  style: Theme.of(context).textTheme.headline1,
-                ),
-              ),
-              Text(
-                'Light ' +
-                    Theme.of(context)
-                        .textTheme
-                        .headline1!
-                        .fontSize!
-                        .floor()
-                        .toString(),
-                style: Theme.of(context).textTheme.bodyText2,
-              ),
-            ],
-          ),
-          _line(),
-          Row(
-            children: [
-              Expanded(
-                child: Text(
-                  'H2',
-                  style: Theme.of(context).textTheme.headline2,
-                ),
-              ),
-              Text(
-                'Light ' +
-                    Theme.of(context)
-                        .textTheme
-                        .headline2!
-                        .fontSize!
-                        .floor()
-                        .toString(),
-                style: Theme.of(context).textTheme.bodyText2,
-              ),
-            ],
-          ),
-          Text(
-            'Used for the date in the dialog shown by [shownDatePicker].',
-            style: Theme.of(context).textTheme.bodyText2,
-          ),
-          _line(),
-          Row(
-            children: [
-              Expanded(
-                child: Text(
-                  'H3',
-                  style: Theme.of(context).textTheme.headline3,
-                ),
-              ),
-              Text(
-                'Regular ' +
-                    Theme.of(context)
-                        .textTheme
-                        .headline3!
-                        .fontSize!
-                        .floor()
-                        .toString(),
-                style: Theme.of(context).textTheme.bodyText2,
-              ),
-            ],
-          ),
-          _line(),
-          Row(
-            children: [
-              Expanded(
-                child: Text(
-                  'H4',
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-              ),
-              Text(
-                'Regular ' +
-                    Theme.of(context)
-                        .textTheme
-                        .headline4!
-                        .fontSize!
-                        .floor()
-                        .toString(),
-                style: Theme.of(context).textTheme.bodyText2,
-              ),
-            ],
-          ),
-          _line(),
-          Row(
-            children: [
-              Expanded(
-                child: Text(
-                  'H5',
-                  style: Theme.of(context).textTheme.headline5,
-                ),
-              ),
-              Text(
-                'Regular ' +
-                    Theme.of(context)
-                        .textTheme
-                        .headline5!
-                        .fontSize!
-                        .floor()
-                        .toString(),
-                style: Theme.of(context).textTheme.bodyText2,
-              ),
-            ],
-          ),
-          Text(
-            'Used for large text in the dialogs (e.g., the month and year in the the dialog shown by [shownDatePicker]).',
-            style: Theme.of(context).textTheme.bodyText2,
-          ),
-          _line(),
-          Row(
-            children: [
-              Expanded(
-                child: Text(
-                  'H6',
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-              ),
-              Text(
-                'Medium ' +
-                    Theme.of(context)
-                        .textTheme
-                        .headline6!
-                        .fontSize!
-                        .floor()
-                        .toString(),
-                style: Theme.of(context).textTheme.bodyText2,
-              ),
-            ],
-          ),
-          Text(
-            'Used for the primary text in app bars and dialogs (e.g., [AppBar.title] and [AlertDialog.title]).',
-            style: Theme.of(context).textTheme.bodyText2,
-          ),
-          _line(),
-          Row(
-            children: [
-              Expanded(
-                child: Text(
-                  'Subtitle1',
-                  style: Theme.of(context).textTheme.subtitle1,
-                ),
-              ),
-              Text(
-                'Regular ' +
-                    Theme.of(context)
-                        .textTheme
-                        .subtitle1!
-                        .fontSize!
-                        .floor()
-                        .toString(),
-                style: Theme.of(context).textTheme.bodyText2,
-              ),
-            ],
-          ),
-          Text(
-            'Used for the primary text in lists (e.g., [ListTile.title]).',
-            style: Theme.of(context).textTheme.bodyText2,
-          ),
-          _line(),
-          Row(
-            children: [
-              Expanded(
-                child: Text(
-                  'Subtitle2',
-                  style: Theme.of(context).textTheme.subtitle2,
-                ),
-              ),
-              Text(
-                'Medium ' +
-                    Theme.of(context)
-                        .textTheme
-                        .subtitle2!
-                        .fontSize!
-                        .floor()
-                        .toString(),
-                style: Theme.of(context).textTheme.bodyText2,
-              ),
-            ],
-          ),
-          Text(
-            'For medium emphasis text that\'s a little smaller than [subtitle1].',
-            style: Theme.of(context).textTheme.bodyText2,
-          ),
-          _line(),
-          Row(
-            children: [
-              Expanded(
-                child: Text(
-                  'BodyText1',
-                  style: Theme.of(context).textTheme.bodyText1,
-                ),
-              ),
-              Text(
-                'Regular ' +
-                    Theme.of(context)
-                        .textTheme
-                        .bodyText1!
-                        .fontSize!
-                        .floor()
-                        .toString(),
-                style: Theme.of(context).textTheme.bodyText2,
-              ),
-            ],
-          ),
-          Text(
-            'Used for emphasizing text that would otherwise be [bodyText2].',
-            style: Theme.of(context).textTheme.bodyText2,
-          ),
-          _line(),
-          Row(
-            children: [
-              Expanded(
-                child: Text(
-                  'BodyText2',
-                  style: Theme.of(context).textTheme.bodyText2,
-                ),
-              ),
-              Text(
-                'Regular ' +
-                    Theme.of(context)
-                        .textTheme
-                        .bodyText2!
-                        .fontSize!
-                        .floor()
-                        .toString(),
-                style: Theme.of(context).textTheme.bodyText2,
-              ),
-            ],
-          ),
-          Text(
-            'The default text style for [Material].',
-            style: Theme.of(context).textTheme.bodyText2,
-          ),
-          _line(),
-          Row(
-            children: [
-              Expanded(
-                child: Text(
-                  'Button',
-                  style: Theme.of(context).textTheme.button,
-                ),
-              ),
-              Text(
-                'Medium ' +
-                    Theme.of(context)
-                        .textTheme
-                        .button!
-                        .fontSize!
-                        .floor()
-                        .toString(),
-                style: Theme.of(context).textTheme.bodyText2,
-              ),
-            ],
-          ),
-          Text(
-            'Used for text on [ElevatedButton], [TextButton] and [OutlinedButton].',
-            style: Theme.of(context).textTheme.bodyText2,
-          ),
-          _line(),
-          Row(
-            children: [
-              Expanded(
-                child: Text(
-                  'Caption',
-                  style: Theme.of(context).textTheme.caption,
-                ),
-              ),
-              Text(
-                'Regular ' +
-                    Theme.of(context)
-                        .textTheme
-                        .caption!
-                        .fontSize!
-                        .floor()
-                        .toString(),
-                style: Theme.of(context).textTheme.bodyText2,
-              ),
-            ],
-          ),
-          Text(
-            'Used for auxiliary text associated with images.',
-            style: Theme.of(context).textTheme.bodyText2,
-          ),
-          _line(),
-          Row(
-            children: [
-              Expanded(
-                child: Text(
-                  'Overline',
-                  style: Theme.of(context).textTheme.overline,
-                ),
-              ),
-              Text(
-                'Regular ' +
-                    Theme.of(context)
-                        .textTheme
-                        .overline!
-                        .fontSize!
-                        .floor()
-                        .toString(),
-                style: Theme.of(context).textTheme.bodyText2,
-              ),
-            ],
-          ),
-          Text(
-            'Typically used for captions or to introduce a (larger) headline.',
-            style: Theme.of(context).textTheme.bodyText2,
-          ),
+          for (var i = 0; i < itemData.length; i++) _buildText(itemData[i]),
         ],
       ),
     );
   }
+}
+
+class ItemData {
+  String title;
+  String description;
+  TextStyle? style;
+  //final, const, var ìn flutter
+
+  ItemData(this.title, this.description, this.style);
 }
